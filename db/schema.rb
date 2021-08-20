e38376803786b318c550fdb718694be74008a4cd
TakMor-11 <takmorfeelin@gmail.com>
2021-08-20 21:54:28 +0900

    モデルの作成まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_022419) do
+ActiveRecord::Schema.define(version: 2021_08_20_114238) do
+
+  create_table "manages", force: :cascade do |t|
+    t.string "task"
+    t.date "due"
+    t.bigint "totaltime"
+    t.integer "hurry"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
