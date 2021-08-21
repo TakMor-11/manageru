45ef536298a11fd1670f81d63798f9fbf4e0ea05
TakMor-11 <takmorfeelin@gmail.com>
2021-08-21 12:05:17 +0900

    ユーザの設定内容マイグレーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :manages, dependent: :destroy
+  validates :name, allow_blank: true
+  validates :hpd, presence: true
+  validates :act, presence: true
+  validates :rest, presence:true
 end
