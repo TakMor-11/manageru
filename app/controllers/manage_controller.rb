af3b864e765ff54b4a21f2f8837c65af78380fcc
TakMor-11 <takmorfeelin@gmail.com>
2021-08-23 21:05:23 +0900

    エラーに苦しむ我

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -8,5 +8,29 @@ class ManageController < ApplicationController
     def start
 
     end
+    
+    def new
+        @manage = Manage.new
+    end
+
+    def create
+        manage = Manage.new(manage_params)
+        if manage.save
+            redirect_to :action => "taskshow"
+        else
+            redirect_to :action => "new"
+        end
+    end
+    
+    def taskshow
+
+    end
+
+    private
+    def manage_params
+        params.require(:manage).permit(:task, :due, :totaltime, :hurry)
+    end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "manage/index" => "manage#index"
-  get "manage/start" => "manage#start"
-  root "manage#index"
   resources :users, only: [:show]
+  
+  resources :manage
+  get "manage/start" => "manage#start"
+  get "manage/taskshow" => "manage#taskshow"
+  root "manage#index"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
